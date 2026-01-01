c60d11dfec6377a921b8c4beb0efca14fdf11ca6
vxxxxc <veper1234@icloud.com>
2026-01-01 16:26:09 +0800

    add CEI pattern in the claim function , for prevent reentrancy attack or multiple claim

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -14,19 +14,29 @@ contract MerkleAirdrop {
     bytes32 private immutable i_merkleRoot;
     IERC20 private immutable i_airdropToken;
 
+    mapping(address claimer => bool claimed) private s_hasClaimed;
+
     event Claim(address indexed account, uint256 amount);
 
     error MerkleAirdrop__InvalidProof();
+    error MerkleAirdrop__AlreadyClaimed();
 
     constructor(bytes32 merkleRoot, IERC20 airdropToken) {
         i_merkleRoot = merkleRoot;
         i_airdropToken = airdropToken;
     }
 
+    // WARN: This function need to be follow CEI(Checks-Effects-Interactions) pattern to prevent reentrancy attack
     function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        // NOTE: CHECKS
+        if (s_hasClaimed[account] == true) {
+            revert MerkleAirdrop__AlreadyClaimed();
+        }
+
         // calculate using the account and the amount , the hash -> leaf node
-        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount)))); // WARN: leaf cannot be bytes64 or longer , also don't use other hash function than keccak256 , as mentioned in @openzeppelin warning in MerkleProof
-        // WARN: also keccak256 hashed the leaf twice time for prevent preimage attack
+        // WARN: leaf cannot be bytes64 or longer , also don't use other hash function than keccak256 , as mentioned in @openzeppelin warning in MerkleProof
+        // also keccak256 hashed the leaf twice time for prevent preimage attack
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
         if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
             revert MerkleAirdrop__InvalidProof();
         }
